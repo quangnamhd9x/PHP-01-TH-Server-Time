5f6337679ad4b7a48decebe4068c0c8188e342bf
iTRobert <quangnamhd9x@gmail.com>
2020-09-06 21:41:48 +0700

    update

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Time Now</title>
+</head>
+<body>
+    <h1> Time: <?php echo date('Y-M-d h:m:s'); ?></h1>
+    <?php date_default_timezone_get('Asia/Ho-Chi_Minh') ?>
+</body>
+</html>
+<?php
+echo "xin chao";
